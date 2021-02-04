4fe29dacce2e5bda96c642b53ea5d1cb3e2730ee
karaBASSbaraBASS <evgen.mikhalov@gmail.com>
2021-02-05 01:49:40 +0200

    navigation to the main page

---
+++ Parabank.UITests/ParabankWebAppShould.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Parabank.UITests
+{
+    public class ParabankWebAppShould
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationPage() 
+        {
+            using (IWebDriver driver = new ChromeDriver()) 
+            {
+                driver.Navigate().GoToUrl("https://parabank.parasoft.com/parabank/about.htm");
+            }
+        }
+    }
+}
